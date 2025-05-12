9a71064cc374b1989d41d91d9980648e4867da56
sysadmin <sysadmin@m6500db-41.testlab.local>
2025-05-12 14:58:01 +0200

    requests hinzugefügt und Beispielskript erstellt(main.py).

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import requests
+response = requests.get("https://api.github.com")
+print("StatusCode: ",response.status_code)
+
 def main():
-    print("Hello from py-venv-proj1, created by UV !")
+    print("Hello from py-venv-proj1, created by UV ! from main()")
 
 
 if __name__ == "__main__":
